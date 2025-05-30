065e87a153156c3f31a0b8e590419e5952631975
Oleg <oleg-sokol.sokolov2016@yandex.ru>
2025-05-30 19:28:36 +0500

    add id

--- backend/pyd/create_models.py
+++ backend/pyd/create_models.py
@@ -4,9 +4,6 @@ from pydantic import BaseModel, Field, EmailStr, field_validator
 from pyd import *
 import re
 
-
-# === Обновление моделей для создания ===
-
 class GenreCreate(GenreBase):
     pass
 
@@ -45,23 +42,20 @@ class RatingCreate(RatingBase):
     user_id: int = Field(..., example=1)
     comic_id: int = Field(..., example=1)
 
-# === Модели для создания томов, глав и страниц ===
-
 class PageCreate(BaseModel):
-    volume_id: int = Field(..., example=1)  # Идентификатор тома
-    chapter_id: int = Field(..., example=1)  # Идентификатор главы
-    number: int = Field(..., example=1)  # Номер страницы
-    image_url: str = Field(..., example="comics/Комикс 1/comic/vl1/ch1/1.jpg")  # Путь к изображению страницы
+    volume_id: int = Field(..., example=1)  
+    chapter_id: int = Field(..., example=1) 
+    number: int = Field(..., example=1)  
+    image_url: str = Field(..., example="comics/Комикс 1/comic/vl1/ch1/1.jpg")
 
 class ChapterCreate(BaseModel):
-    volume_id: int = Field(..., example=1)  # Идентификатор тома
-    number: int = Field(..., example=1)  # Номер главы
-    title: str = Field(..., example="Глава 1")  # Название главы
+    volume_id: int = Field(..., example=1)  
+    number: int = Field(..., example=1)  
+    title: str = Field(..., example="Глава 1") 
 
-    # Новое поле для страниц
-    pages: List[PageCreate] = []  # Список страниц в главе
+    pages: List[PageCreate] = []
 
 class VolumeCreate(BaseModel):
-    number: int = Field(..., example=1)  # Номер тома
-    chapters: List[ChapterCreate] = []  # Список глав в томе
+    number: int = Field(..., example=1)
+    chapters: List[ChapterCreate] = []
 
