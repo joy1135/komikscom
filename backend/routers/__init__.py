41b9af8ba04753853ded83c09b88ebbdf9df813d
Oleg <oleg-sokol.sokolov2016@yandex.ru>
2025-06-02 14:33:49 +0500

    add comm and rating

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -3,3 +3,4 @@ from .aftor_router import router as aftor_router
 from .user_router import router as user_router
 from .genre_router import router as genre_router
 from .comic_create_router import router as comic_create_router
+from .comment_router import router as comment_router
